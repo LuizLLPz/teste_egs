3d1cc93144b19d1179baaf640e47107bcae7d3cf
luizllpz <luizllp321@gmail.com>
2022-11-02 00:28:31 -0300

    Adicionando funcionalidade para calcular e mostrar desconto de produto (inicial)

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿var CalculaDesconto = (double preco, double porcentagem) 
+    => preco - (preco * porcentagem / 100);
 
-Console.WriteLine("Hello, World!");
+// Main method
+Console.WriteLine("Black Friday! Produtos com desconto de 60%");
+
+double preco = Convert.ToDouble(Console.ReadLine());
+double desconto = CalculaDesconto(preco, 60);
+
+Console.WriteLine("Produto custava: " + preco);
+Console.WriteLine($"Com o desconto: {preco - desconto:##.##}");
+Console.WriteLine($"Valor do desconto: {desconto:##.##}");
